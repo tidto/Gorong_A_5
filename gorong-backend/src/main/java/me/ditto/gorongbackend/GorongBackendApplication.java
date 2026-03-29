47c22448c4c1cf1819efe5a6801cb00cbf512403
tidto <itnttoremayrain@gmail.com>
2026-03-29 11:02:45 +0900

    260329_mk_backendFile

---
+++ gorong-backend/src/main/java/me/ditto/gorongbackend/GorongBackendApplication.java
@@ -0,0 +1,13 @@
+package me.ditto.gorongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GorongBackendApplication.class, args);
+    }
+
+}
---
+++ gorong-backend/src/test/java/me/ditto/gorongbackend/GorongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ditto.gorongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
