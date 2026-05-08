971eb3918370891b74965d4d310f1788d1a9f1b3
tidto <139437094+tidto@users.noreply.github.com>
2026-05-08 21:52:25 +0900

    Update UserProfile.java

--- gorong-back/src/main/java/com/gorong/backend/domain/user/entity/UserProfile.java
+++ gorong-back/src/main/java/com/gorong/backend/domain/user/entity/UserProfile.java
@@ -35,7 +35,7 @@ public class UserProfile {
     @Column(name = "base_address", columnDefinition = "TEXT")
     private String baseAddress;
 
-    @Column(name = "base_location")
+    @Column(name = "base_location", columnDefinition = "geometry(Point, 4326)")
     private Point baseLocation;
 
     // @Builder.Default 와 = 38.5 를 삭제했습니다! (생성자에서 알아서 해주니까요)
