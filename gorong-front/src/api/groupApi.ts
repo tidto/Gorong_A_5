46fa610578c396da5b9a9ccde8292eee512bc307
tidto <139437094+tidto@users.noreply.github.com>
2026-05-13 20:17:26 +0900

    Update groupApi.ts

--- gorong-front/src/api/groupApi.ts
+++ gorong-front/src/api/groupApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/groups';
+const API_URL = 'http://98.84.85.31/api/groups';
 
 export const fetchGroups = async () => {
     const response = await axios.get(API_URL);
