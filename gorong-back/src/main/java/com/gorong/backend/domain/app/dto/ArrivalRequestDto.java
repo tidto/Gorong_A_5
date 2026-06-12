53bc988981ad0587a5fb1dd6f3fbf19dc8f50832
tidto <itnttoremayrain@gmail.com>
2026-06-12 13:16:41 +0900

    tester

--- gorong-back/src/main/java/com/gorong/backend/domain/app/dto/ArrivalRequestDto.java
+++ gorong-back/src/main/java/com/gorong/backend/domain/app/dto/ArrivalRequestDto.java
@@ -3,14 +3,13 @@ package com.gorong.backend.domain.app.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ArrivalRequestDto {
-    @JsonProperty("venueId")
     private String venueId;
-    @JsonProperty("lat")
     private double lat;
-    @JsonProperty("lng")
     private double lng;
 }
