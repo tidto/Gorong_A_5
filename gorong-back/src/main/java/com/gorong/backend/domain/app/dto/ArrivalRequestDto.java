58f62cc401d0f2222eb87ca3910dde3c2d977831
tidto <itnttoremayrain@gmail.com>
2026-06-12 13:06:40 +0900

    test2

--- gorong-back/src/main/java/com/gorong/backend/domain/app/dto/ArrivalRequestDto.java
+++ gorong-back/src/main/java/com/gorong/backend/domain/app/dto/ArrivalRequestDto.java
@@ -1,12 +1,16 @@
 package com.gorong.backend.domain.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ArrivalRequestDto {
+    @JsonProperty("venueId")
     private String venueId;
-    private double lat;   // 앱에서 보낸 현재 위치
+    @JsonProperty("lat")
+    private double lat;
+    @JsonProperty("lng")
     private double lng;
 }
